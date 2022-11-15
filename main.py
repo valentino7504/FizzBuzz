54b952935d739f38297f9e5d8a3e0a8d30c9ced7
Edwin Ade <70894222+valentino7504@users.noreply.github.com>
2022-11-15 15:20:47 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+
+
+for num in range(1, 101):
+    print(fizzbuzz(num))
